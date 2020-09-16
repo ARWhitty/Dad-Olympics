46f02d660945cc4f5e2ce4dea46c0c4c1c3faaeb
Unknown <slimeballgamesofficial@gmail.com>
2020-09-15 21:46:15 -0600

    Worked on the Sandbox

--- Dad Olympics Game/Assets/Scripts/KnockBack.cs
+++ Dad Olympics Game/Assets/Scripts/KnockBack.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -27,3 +28,34 @@ public class KnockBack : MonoBehaviour
         }
     }
 }
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnockBack : MonoBehaviour
+{
+    public short kBForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Vector3 direction = other.transform.position - transform.position;
+            direction = direction.normalized + new Vector3(0,1,-3);
+            other.gameObject.GetComponent<PlayerMovement>().KnockBack(direction * kBForce);
+        }
+    }
+}
+>>>>>>> Stashed changes
