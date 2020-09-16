2c815e34e60850a8659164380021db86697c56ee
Addison <addyshuppy@gmail.com>
2020-09-16 00:06:34 -0600

    Deleted Extra Project, merged kieran scene

--- Dad Olympics Game/Assets/Scripts/KnockBack.cs
+++ Dad Olympics Game/Assets/Scripts/KnockBack.cs
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -28,34 +27,3 @@ public class KnockBack : MonoBehaviour
         }
     }
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class KnockBack : MonoBehaviour
-{
-    public short kBForce;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void OnTriggerEnter(Collider other)
-    {
-        if(other.tag == "Player")
-        {
-            Vector3 direction = other.transform.position - transform.position;
-            direction = direction.normalized + new Vector3(0,1,-3);
-            other.gameObject.GetComponent<PlayerMovement>().KnockBack(direction * kBForce);
-        }
-    }
-}
->>>>>>> Stashed changes
--- Dad Olympics Game/Assets/Scripts/Oscillator.cs
+++ Dad Olympics Game/Assets/Scripts/Oscillator.cs
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -48,54 +47,3 @@ public class Oscillator : MonoBehaviour
         }
     }
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Oscillator : MonoBehaviour
-{
-    public Vector3 lesserXPoint, greaterXPoint;
-    public short xMin, xMax;
-    public short speed;
-    public bool toPointA;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (toPointA) {
-            if (gameObject.transform.position.x < lesserXPoint.x)
-            {
-                toPointA = false;
-            }
-            if (gameObject.transform.position.x < xMin)
-            {
-                gameObject.GetComponent<Rigidbody>().velocity = new Vector3(-gameObject.GetComponent<Rigidbody>().velocity.x, 0, 0);
-            }
-            else
-            {
-                gameObject.GetComponent<Rigidbody>().velocity = Vector3.MoveTowards(transform.position, lesserXPoint, 500).normalized * speed;
-            }
-        } else
-        {
-            if (gameObject.transform.position.x > greaterXPoint.x)
-            {
-                toPointA = true;
-            }
-            if (gameObject.transform.position.x > xMax)
-            {
-                gameObject.GetComponent<Rigidbody>().velocity = new Vector3(-gameObject.GetComponent<Rigidbody>().velocity.x, 0, 0);
-            } else
-            {
-                gameObject.GetComponent<Rigidbody>().velocity = Vector3.MoveTowards(transform.position, greaterXPoint, 500).normalized * speed;
-            }
-        }
-    }
-}
->>>>>>> Stashed changes
