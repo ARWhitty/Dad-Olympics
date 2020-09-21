c34696c55c457c30d8c48a5c490c1e9950811498
Unknown <slimeballgamesofficial@gmail.com>
2020-09-21 00:02:11 -0600

    First attempt at pushing the cardboard popup

---
+++ Dad Olympics Game/Assets/Scripts/PopUpCutout.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpCutout : MonoBehaviour
+{
+    public Quaternion currentPos;
+    public Quaternion rotationAngle;
+    public float speed;
+    public bool Standing;
+    public float timer;
+    private float countdown;
+    private short initialForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdown = timer;
+        initialForce = this.GetComponent<KnockBack>().kBForce;
+           currentPos = this.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(countdown);
+         countdown -= Time.deltaTime;
+        if (countdown < 0) {
+            Standing = !Standing;
+            countdown = timer;
+            }
+
+        if (Standing)
+        {
+            this.GetComponent<KnockBack>().kBForce = 0;
+            this.GetComponent<BoxCollider>().enabled = false;
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotationAngle, Time.deltaTime * speed);
+        }
+        else
+        {
+            this.GetComponent<KnockBack>().kBForce = initialForce;
+            this.GetComponent<BoxCollider>().enabled = true;
+            transform.rotation = Quaternion.Slerp(transform.rotation, currentPos, Time.deltaTime * speed);
+        }
+
+    }
+
+}
