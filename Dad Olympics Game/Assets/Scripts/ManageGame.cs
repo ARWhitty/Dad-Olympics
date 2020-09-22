969a0e83ac4b35bf381a4e5a748b6dc7492dba8a
KIERAN BORLAND <u1136227@umail.utah.edu>
2020-09-21 20:22:33 -0600

    Implemented a timer, reset, and win condition

---
+++ Dad Olympics Game/Assets/Scripts/ManageGame.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManageGame : MonoBehaviour
+{
+    public bool isTimerCountDown;
+
+    public float startTime;
+
+    public float currentTime;
+
+    public GameObject player;
+
+    private Vector3 startPos;
+
+    public Text gameTimerText;
+
+    private bool gameRunning;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startTime;
+        startPos = player.transform.position;
+        gameRunning = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameRunning)
+        {
+            if (currentTime <= 0)
+            {
+                ResetGame();
+            }
+            if (isTimerCountDown)
+            {
+                currentTime -= Time.deltaTime;
+                gameTimerText.text = "Time Remaining: " + currentTime;
+            }
+            else
+            {
+                currentTime += Time.deltaTime;
+                gameTimerText.text = "Current Time: " + currentTime;
+            }
+
+
+
+
+            if (Input.GetKeyDown("r"))
+            {
+                ResetGame();
+            }
+        }
+
+    }
+
+    private void ResetGame()
+    {
+        currentTime = startTime;
+        player.transform.position = startPos;
+    }
+
+    void OnCollisionEnter(Collision obj)
+    {
+        Debug.Log("Trigger end");
+        gameRunning = false;
+
+    }
+}
+
+
+
