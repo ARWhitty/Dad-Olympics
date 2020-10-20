d4b07a2aadd61449693b7134ca344cc5246426d7
Unknown <slimeballgamesofficial@gmail.com>
2020-10-20 16:10:16 -0600

    Almost done with"The Block"

---
+++ Dad Olympics Game/Assets/Scripts/FireHydrantKB.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireHydrantKB : MonoBehaviour
+{
+    public bool spraying = false;
+    public float speed;
+    public float delay;
+    public float length;
+    private float timer;
+
+    public float size;
+
+    public GameObject water;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
