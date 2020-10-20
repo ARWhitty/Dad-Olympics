455a7ece0904fa1ab085a73e4f6dc0847709432e
Addison <addyshuppy@gmail.com>
2020-10-19 20:12:14 -0600

    Added Different Camera Demos

---
+++ Dad Olympics Game/Assets/Scripts/CameraMoveFG.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMoveFG : MonoBehaviour
+{
+    public float rotationSpeedLateral, rotationSpeedVertical;
+    public Transform target, player;
+    float mouseX, mouseY;
+    public float minY, maxY, playerRotation;
+
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void LateUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        playerRotation += horizontalInput;
+        Control(playerRotation);
+        //For editor only!
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    void Control(float horizontalInput)
+    {
+        mouseX += Input.GetAxis("Mouse X") * rotationSpeedLateral;
+        mouseY -= Input.GetAxis("Mouse Y") * rotationSpeedVertical;
+        mouseY = Mathf.Clamp(mouseY, minY, maxY);
+
+        transform.LookAt(target);
+
+        target.rotation = Quaternion.Euler(mouseY, mouseX + horizontalInput, 0);
+    }
+}
