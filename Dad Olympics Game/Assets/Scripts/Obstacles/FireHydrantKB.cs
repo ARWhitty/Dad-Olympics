22811fb7bd47b09d64bfa18c40bfaf835d212e9b
Unknown <slimeballgamesofficial@gmail.com>
2020-10-26 00:51:23 -0600

    Finished the block

--- Dad Olympics Game/Assets/Scripts/Obstacles/FireHydrantKB.cs
+++ Dad Olympics Game/Assets/Scripts/Obstacles/FireHydrantKB.cs
@@ -1,28 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FireHydrantKB : MonoBehaviour
 {
-    public bool spraying = false;
-    public float speed;
+    public bool Spraying = false;
     public float delay;
     public float length;
     private float timer;
+    public short force;
 
-    public float size;
-
-    public GameObject water;
+    public GameObject waterKB;
+    public ParticleSystem WaterEffect;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = delay;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        delay -= Time.deltaTime;
+        if (delay < 0)
+        {
+            Spraying = !Spraying;
+            delay = timer;
+        }
+
+        if (Spraying)
+        {
+            waterKB.SetActive(true);
+            waterKB.GetComponent<KnockBack>().kBForce = force;
+            WaterEffect.Play();
+            
+
+        }
+        else
+        {
+            waterKB.SetActive(false);
+            waterKB.GetComponent<KnockBack>().kBForce = 0;
+            WaterEffect.Stop();
+        }
     }
 }
---
+++ Dad Olympics Game/Assets/Scripts/Obstacles/TrikeCircle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrikeCircle : MonoBehaviour
+{
+
+    //Place a waypoint marker here
+    public GameObject center;
+    public short speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(center.transform.position, Vector3.up, speed * Time.deltaTime);
+    }
+}
