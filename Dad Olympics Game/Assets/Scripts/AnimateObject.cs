37366cdfec9a79d9c7841c176743bb87235307be
KIERAN BORLAND <u1136227@umail.utah.edu>
2020-11-08 17:05:41 -0700

    Added script to animate objects

--- Dad Olympics Game/Assets/Scripts/AnimateObject.cs
+++ Dad Olympics Game/Assets/Scripts/AnimateObject.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class AnimateObject : MonoBehaviour
 {
+    public Material[] animationMaterials;
+    public float swapTime;
+    public GameObject objectToAnimate;
+
+    private int materialCounter;
+    private float timeCount;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(animationMaterials.Length > 0)
+        {
+            objectToAnimate.GetComponent<MeshRenderer>().material = animationMaterials[0];
+            materialCounter++;
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeCount += Time.deltaTime;
+        if(timeCount >= swapTime)
+        {
+            if(animationMaterials.Length <= materialCounter)
+            {
+                objectToAnimate.GetComponent<MeshRenderer>().material = animationMaterials[0];
+                materialCounter = 1;
+            }
+            else
+            {
+                objectToAnimate.GetComponent<MeshRenderer>().material = animationMaterials[materialCounter];
+                materialCounter++;
+            }
+            timeCount = 0;
+        }
     }
 }
