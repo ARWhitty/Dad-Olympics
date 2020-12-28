b392951e74d2b9fd266c3eabb39b7ddf2256490b
Scott Mattes <scottmattes90@gmail.com>
2020-12-28 12:26:55 -0700

    Minor downtown update, imported new models
    Imported new dumpster and garbage bag

---
+++ Dad Olympics Game/Assets/Scripts/Interaction.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using static UnityEngine.InputSystem.InputAction;
+using UnityEngine.SceneManagement;
+
+public class Interaction : MonoBehaviour
+
+{
+    public GameObject Object_active;
+    public GameObject Object_inactive;
+
+    private bool interactPressed = false;
+
+    public void Update()
+    {
+        if (interactPressed)
+        {
+            Object_active.SetActive(false);
+            Object_inactive.SetActive(true);
+        }
+    }
+
+    public void OnInteract(CallbackContext context)
+    {
+        if (context.performed)
+        {
+            interactPressed = true;
+        }
+        else
+        {
+            interactPressed = false;
+        }
+    }
+}
