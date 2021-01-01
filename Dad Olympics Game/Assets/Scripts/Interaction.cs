c7c2620f65f0f355e4b7f1336f671ac0f8872dbe
Scott Mattes <scottmattes90@gmail.com>
2021-01-01 14:26:19 -0700

    Power Bill update
    Blocked out the house. Objects that are "on" will contribute to the overall power bill total

--- Dad Olympics Game/Assets/Scripts/Interaction.cs
+++ Dad Olympics Game/Assets/Scripts/Interaction.cs
@@ -14,6 +14,13 @@ public class Interaction : MonoBehaviour
     public GameObject Object_active;
     public GameObject Object_inactive;
 
+    public int BillTotal = 0;
+    public int TimerDelayAmount = 1;
+
+    public Text BillTotalText;
+
+    protected float CashTimer;
+
     private bool interactPressed = false;
 
     public void Update()
@@ -22,11 +29,25 @@ public class Interaction : MonoBehaviour
         {
             
         }
+
+        if (!interactPressed)
+        {
+            CashTimer += Time.deltaTime;
+
+            if (CashTimer >= TimerDelayAmount)
+            {
+                CashTimer = 0f;
+                BillTotal++;
+                BillTotalText.text = "Power Bill: $" + BillTotal;
+
+            }
+        }
     }
 
     public void ToggleVisual()
     {
         Object_active.SetActive(false);
         Object_inactive.SetActive(true);
+        interactPressed = true;
     }
 }
---
+++ Dad Olympics Game/Assets/Scripts/PowerBillCash.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerBillCash : MonoBehaviour
+{
+    public int BillTotal = 0;
+    public int TimerDelayAmount = 1;
+
+    public Text BillTotalText;
+
+    protected float CashTimer;
+
+    void Update()
+    {
+        CashTimer += Time.deltaTime;
+
+        if (CashTimer >= TimerDelayAmount)
+        {
+            CashTimer = 0f;
+            BillTotal++;
+            BillTotalText.text = "Power Bill: $" + BillTotal;
+
+        }
+    }
+}
